7489a4691eefc7ecac12b18b27d357a80f83c24c
yogeshoza333335 <yogeshoza33333@gmail.com>
2024-07-11 09:28:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+console.log("Starting");
+
+//github command
+
+// echo "# practice123" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Yogesh-Oza/practice123.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/Yogesh-Oza/practice123.git
+// git branch -M main
+// git push -u origin main
